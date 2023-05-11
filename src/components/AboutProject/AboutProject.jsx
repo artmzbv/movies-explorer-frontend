d41f96686174dc0a2aa5cdcd0df458e4fdaf243d
Artem Zubov <artmzbv@gmail.com>
2023-05-11 19:33:36 +0300

    new_commit

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -5,10 +5,10 @@ function AboutProject(){
         <section className="project" id="project">
         <h1 className="project__title">О проекте</h1>
         <div className="project__description">
-        <h2 className="project__subtitle">Дипломный проект включал 5 этапов</h2>
-        <h2 className="project__subtitle">На выполнение диплома ушло 5 недель</h2>
-        <p className="project__text"> Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-        <p className="project__text"> У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <h2 className="project__subtitle project__subtitle_first">Дипломный проект включал 5 этапов</h2>
+        <p className="project__text project__text_first"> Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        <h2 className="project__subtitle project__subtitle_second">На выполнение диплома ушло 5 недель</h2>
+        <p className="project__text project__text_second"> У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
         </div>
         <div className="project__timing">
         <div className="project__viz project__viz_first">1 неделя</div>
--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -6,7 +6,7 @@ function Techs() {
         <section className="techs" id="techs">
         <h1 className="techs__title">Технологии</h1>
         <h2 className="techs__subtitle">7 технологий</h2>
-        <h3 className="techs__description">На курсе веб-разработки мы освоили технологии, которые применили<br></br> в дипломном проекте.</h3>
+        <h3 className="techs__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</h3>
         <ul className="techs__list">
         <li className="techs__text">HTML</li>
         <li className="techs__text">CSS</li>
