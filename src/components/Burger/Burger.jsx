ea0c2f4e7f429cac2c7a3d28d83a5630bd32736c
Artem Zubov <artmzbv@gmail.com>
2023-05-12 07:53:17 +0300

    new_commit

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -8,6 +8,7 @@ function Burger({isOpened, popupMenuClosed}){
     // const burgerOpened = isOpened ? "burger__menu" : "burger__menu_hidden {burgerOpened};
     return(
         <section className={isOpened ? "burger__menu" : "burger__menu_hidden"}>
+        <div className="burger__opacity">
          <button className="burger__button" type="button" area-label="закрыть окно" onClick={popupMenuClosed}><img src={exit} alt="закрыть"/></button>
         <div className="burger__container">
         <div className="burger__content">
@@ -15,7 +16,6 @@ function Burger({isOpened, popupMenuClosed}){
         <NavLink to="/movies" className="burger__link" onClick={popupMenuClosed}>Фильмы</NavLink>
         <NavLink to="/saved" className="burger__link" onClick={popupMenuClosed}>Сохраненные фильмы</NavLink>
         </div>
-        <div>   
         <NavLink to="/profile" className="burger__account" onClick={popupMenuClosed}>Аккаунт</NavLink>
         </div>
         </div>
